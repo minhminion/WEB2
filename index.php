7bd703638cc58023b9e016df4a05970315ea3f38
minhminion <46214292+minhminion@users.noreply.github.com>
2019-03-18 17:05:40 +0700

    WEB2

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title  -->
+    <title>M2M - Gaming Gear Shop </title>
+
+    <!-- Favicon  -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+    
+    <link rel="stylesheet" href="./css/aos.css">
+    <!-- Core Style CSS -->
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <?php
+        include("header.php");
+        include("topItem.php");
+        include("newproduct.php");
+        include("brand.php");
+        include("footer.php");
+    ?>
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="js/plugins.js"></script>
+    <!-- Classy Nav js -->
+    <script src="js/classy-nav.min.js"></script>
+    <!--- AOS JS --->
+    <script src="js/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    <!-- Active js -->
+    <script src="js/script.js"></script>
+    <!--------------->
+    <script src="./js/jquery.twbsPagination.min.js"></script>
+
+</body>
+</html>
