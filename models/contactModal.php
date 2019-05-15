b1e7fd382323c3c3bb971d23fc954faa6d74e068
nahntvt <48850767+nahntvt@users.noreply.github.com>
2019-05-15 19:30:22 +0700

    bla bla

--- models/contactModal.php
+++ models/contactModal.php
@@ -5,13 +5,13 @@
         </div>
 
         <div class="contact-info">
-            <h2>How to Find Us</h2>
-            <p>Mauris viverra cursus ante laoreet eleifend. Donec vel fringilla ante. Aenean finibus velit id urna vehicula, nec maximus est sollicitudin.</p>
+            <h2>Thông tin Liên Hệ</h2>
+            <p>M2N luôn phấn đấu, nỗ lực không ngừng để đem lại cho người tiêu dùng những sản phẩm công nghệ tiên tiến, và đã trở thành một trong những thương hiệu đáng tin cậy và được yêu thích nhất. M2N luôn cam kết cung cấp hàng chính hãng từ các nhà sản xuất, chất lượng luôn được đảm bảo và mức giá phù hợp trong môi trường cạnh tranh.</p>
 
             <div class="contact-address mt-50">
-                <p><span>address:</span> 10 Suffolk st Soho, London, UK</p>
-                <p><span>telephone:</span> +12 34 567 890</p>
-                <p><a href="mailto:contact@essence.com">contact@essence.com</a></p>
+                <p><span>địa chỉ:</span> 273 An Dương Vương, Phường 3, Quận 5, TP.HCM</p>
+                <p><span>điện thoại:</span> (028) 38382 664</p>
+                <p>vpkcntt@sgu.edu.vn</p>
             </div>
         </div>
 
